3860e35d066062420d2c2227b9ca67b6db73a335
AimekSS <101892039+AimekSS@users.noreply.github.com>
2023-02-23 00:26:48 +0200

    fixed task2 bug with goto and recursion in task3

--- ReizTechAssignment/Program.cs
+++ ReizTechAssignment/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     public static void Main(string[] args)
     {
         //Task1.GetAngleInBetween();
-        Task2.BranchTreeSetUpService();
+        //Task2.BranchTreeSetUpService();
+        Console.WriteLine("The depth of the tree is: " + Task3.ReturnBranchTreeDepth(Task3.task3Branch));
     }
 }
---
+++ ReizTechAssignment/Task3.cs
@@ -0,0 +1,30 @@
+﻿using ReizTechAssignment.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReizTechAssignment
+{
+    internal class Task3
+    {
+        public static Branch task3Branch = JsonHelper.GetTreeObject();
+
+        public static int ReturnBranchTreeDepth(Branch myBranch)
+        {
+            if (myBranch.branches == null)
+            {
+                return 0;
+            }
+            int maxDepth = 0;
+            foreach(Branch branch in myBranch.branches)
+            {
+                int depth = ReturnBranchTreeDepth(branch);
+                maxDepth = Math.Max(maxDepth, depth);
+            }
+            
+            return maxDepth + 1;
+        }
+    }
+}
